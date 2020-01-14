7665588d1741509755b0d7c15f860759f6b09ac1
Jake <jake@zuusworkforce.com>
2020-01-14 13:10:54 -0700

    Remove extensive debug logging

--- extra/meteor_settings.sh
+++ extra/meteor_settings.sh
@@ -4,7 +4,6 @@
 #
 echo "-----> Adding profile script to set METEOR_SETTING"
 
-# wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq
 curl -o "$APP_CHECKOUT_DIR"/jq -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x "$APP_CHECKOUT_DIR"/jq
 JQ="$APP_CHECKOUT_DIR/jq"
@@ -24,20 +23,12 @@ if $PROD ; then
 else
     # Build the staging settings
     echo "Staging deploy"
-
     # For staging we pre-pend "10" to the version number to make it obvious if a device is 
     # connecting to Staging or Production. 
-    echo "creating staging versions"
     STAGING_VERSION=10$($JQ '.public.version' $APP_CHECKOUT_DIR/server/settings/common.json | tr -d '"')
-    echo $STAGING_VERSION
-    echo `$JQ --version`
-    echo "combining settings"
     # Merge the common and staging specific settings, and include the staging version no.
-    # CONFIG="$APP_CHECKOUT_DIR/server/settings/config.json"
     SETTINGS=`$JQ -c --arg VERSION "$STAGING_VERSION" -s '.[0] * .[1] | .public.version |= $VERSION' $APP_CHECKOUT_DIR/server/settings/common.json $APP_CHECKOUT_DIR/server/settings/staging.json`
 fi
-
-echo $SETTINGS
 
 cat > "$APP_CHECKOUT_DIR"/.profile.d/meteor.sh <<EOF
   #!/bin/bash
